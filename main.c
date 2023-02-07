ac122ed4a717ed1754d8d51457510854aa6bdc8f
Gustavo Bender <gustavo.benderr@gmail.com>
2023-02-07 15:15:36 -0300

    Adicionado o main

---
+++ main.c
@@ -0,0 +1,15 @@
+// #define NDEBUG // disables assert()
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <errno.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+
+	
+
+	return EXIT_SUCCESS;
+}
