1baa04c676c9c113c9515ba1fec0ab7f0cbc2dbf
Gustavo Bender <gustavo.benderr@gmail.com>
2023-02-07 23:23:27 -0300

    Adicao dos metodos acabou e move, e adicao da struct mapa que define o tamanho do mapa e sua matriz que a armazena

--- header.h
+++ header.h
@@ -2,3 +2,12 @@ void liberaMapa();
 void leMapa();
 void alocaMapa();
 void imprimeMapa();
+bool acabou();
+void move(char direcao);
+
+struct mapa
+{
+	char** matriz;
+	int linhas;
+	int colunas;
+};
