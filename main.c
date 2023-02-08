1781d99daf30b5336c26ddc801adfe40a126fdec
Gustavo Bender <gustavo.benderr@gmail.com>
2023-02-07 23:24:27 -0300

    Refatora o codigo para usar os metodos move, acabou e imprimeMapa e utilizar a struct mapa ao inves dos valores soltos

--- main.c
+++ main.c
@@ -7,21 +7,17 @@
 #include <stdio.h>
 #include "header.h"
 
-
-int linhas;
-int colunas;
-char** mapa;
-
+struct mapa m;
 void liberaMapa(){
-	for (size_t i = 0; i < linhas; ++i) {
-		free(mapa[i]);
+	for (size_t i = 0; i < m.linhas; ++i) {
+		free(m.matriz[i]);
 	}
-	free(mapa);
+	free(m.matriz);
 }
 void alocaMapa(){
-	mapa = malloc(sizeof(char*) * linhas);
-	for (size_t i = 0; i < linhas; ++i) {
-		mapa[i] = malloc(sizeof(char) * (colunas+1));
+	m.matriz = malloc(sizeof(char*) * m.linhas);
+	for (size_t i = 0; i < m.linhas; ++i) {
+		m.matriz[i] = malloc(sizeof(char) * (m.colunas+1));
 	}
 }
 
@@ -37,13 +33,13 @@ void leMapa(){
 	}
 
 
-	fscanf(f, "%d %d",&linhas, &colunas);
-	printf("linhas %d colunas %d\n",linhas, colunas);
+	fscanf(f, "%d %d",&m.linhas, &m.colunas);
+	printf("linhas %d colunas %d\n",m.linhas, m.colunas);
 
 	alocaMapa();
 
-	for (size_t i = 0; i < linhas; ++i) {
-		fscanf(f, "%s",mapa[i]);
+	for (size_t i = 0; i < m.linhas; ++i) {
+		fscanf(f, "%s",m.matriz[i]);
 		
 	}
 	
@@ -52,8 +48,50 @@ void leMapa(){
 
 void imprimeMapa(){
 	for (size_t i = 0; i < 5; ++i) {
-		printf("%s\n",mapa[i]);
+		printf("%s\n",m.matriz[i]);
 	}
+}
+
+bool acabou() {
+	return false;
+}
+
+void move(char direcao){
+	int x;
+	int y;
+
+	for (size_t i = 0; i < m.linhas; ++i) {
+		for (size_t j = 0; j < m.colunas; ++j) {
+			if(m.matriz[i][j] == '@') {
+				x = i;
+				y = j;
+				break;
+			}
+			
+		}
+		
+	}
+	switch (direcao) {
+
+		case 'a':
+			m.matriz[x][y-1] = '@';
+
+			break;
+		case 'w':
+			m.matriz[x-1][y] = '@';
+
+			break;
+		case 's':
+			m.matriz[x+1][y] = '@';
+
+			break;
+		case 'd':
+			m.matriz[x][y+1] = '@';
+
+			break;
+	}
+	m.matriz[x][y] = '.';
+
 }
 
 int main(int argc, char *argv[])
@@ -61,7 +99,14 @@ int main(int argc, char *argv[])
 
 	leMapa();
 
-	imprimeMapa();
+	do {
+		imprimeMapa();
+
+		char comando;
+		scanf(" %c", &comando);
+		move(comando);
+
+	} while(!acabou());
 
 
 	liberaMapa();
