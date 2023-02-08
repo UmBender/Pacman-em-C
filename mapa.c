cee26f0b1dac3b63fd748ac16ac29d524a2635e6
Gustavo Bender <gustavo.benderr@gmail.com>
2023-02-07 23:43:28 -0300

    Divisao do codigo colocando as funcoes de mapa nos arquivos mapa.c e suas declaracoes no mapa.h, como tambem o tipo MAPA

---
+++ mapa.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mapa.h"
+
+
+void liberaMapa(MAPA* m){
+	for (size_t i = 0; i < m->linhas; ++i) {
+		free(m->matriz[i]);
+	}
+	free(m->matriz);
+}
+void alocaMapa(MAPA* m){
+	m->matriz = malloc(sizeof(char*) * m->linhas);
+	for (size_t i = 0; i < m->linhas; ++i) {
+		m->matriz[i] = malloc(sizeof(char) * (m->colunas+1));
+	}
+}
+
+
+
+void leMapa(MAPA* m){
+	FILE* f;
+	f = fopen("mapa.txt","r");
+
+	if(f==0){
+		printf("Erro na leitura do mapa\n");
+		exit(1);
+	}
+
+
+	fscanf(f, "%d %d",&m->linhas, &m->colunas);
+	printf("linhas %d colunas %d\n",m->linhas, m->colunas);
+
+	alocaMapa(m);
+
+	for (size_t i = 0; i < m->linhas; ++i) {
+		fscanf(f, "%s",m->matriz[i]);
+		
+	}
+	
+	fclose(f);
+}
+
+void imprimeMapa(MAPA* m){
+	for (size_t i = 0; i < m->linhas; ++i) {
+		printf("%s\n",m->matriz[i]);
+	}
+}
---
+++ mapa.h
@@ -0,0 +1,13 @@
+struct mapa
+{
+	char** matriz;
+	int linhas;
+	int colunas;
+};
+
+typedef struct mapa MAPA;
+
+void liberaMapa(MAPA* m);
+void leMapa(MAPA* m);
+void alocaMapa(MAPA* m);
+void imprimeMapa(MAPA* m);
